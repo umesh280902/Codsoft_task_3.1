f70608a4be45b52652c0ef6f3d95056359b50469
Umesh <umeshkumawat280@gmail.com>
2023-11-27 07:48:51 +0530

    detailed post

--- index.js
+++ index.js
@@ -7,14 +7,34 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { BlogPost } = require('./database/database');
-const corsOptions = {
-  origin: ['https://blogpost-e4d2.onrender.com', 'https://another-allowed-origin.com'],
-  methods: ['GET', 'POST'], // Add other methods as needed
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true, // If you're using cookies or authorization headers
-};
-app.use(cors(corsOptions));
-app.options('*',cors(corsOptions))
+// const corsOptions = {
+//   origin: ['https://blogpost-e4d2.onrender.com', 'https://another-allowed-origin.com'],
+//   methods: ['GET', 'POST'], // Add other methods as needed
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true, // If you're using cookies or authorization headers
+// };
+// app.use(cors(corsOptions));
+// app.options('*',cors(corsOptions))
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://your-frontend.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 app.use(cookieParser());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
